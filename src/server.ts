3ba6e25d741259960dd2ec0015ddd34c621a51a1
Lorgio09 <lorgio.choque09@gmail.com>
2026-09-13 22:11:43 -0400

    resolucion dinamica para index

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 import { join } from 'node:path';
+import { existsSync } from 'node:fs';
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
 const app = express();
 
-// 1. Servir los archivos estáticos (js, css, imágenes)
+// 1. Detección dinámica e infalible de la carpeta de Angular
+const rutaBase = process.cwd(); 
+let browserDistFolder = join(rutaBase, 'dist/fashionstore-web/browser');
+
+// Si por alguna razón Angular no creó la carpeta 'browser', apunta a la raíz del dist
+if (!existsSync(browserDistFolder)) {
+  browserDistFolder = join(rutaBase, 'dist/fashionstore-web');
+}
+
+// 2. Servir los archivos estáticos (js, css, imágenes)
 app.use(
   express.static(browserDistFolder, {
     maxAge: '1y',
@@ -13,13 +22,14 @@ app.use(
   })
 );
 
-// 2. Fallback seguro para Express 5 (Bypass total de SSR)
+// 3. Fallback seguro (Devolver siempre el index.html)
 app.use((req, res) => {
   res.sendFile(join(browserDistFolder, 'index.html'));
 });
 
-// 3. Levantar el servidor
+// 4. Levantar el servidor
 const port = process.env['PORT'] || 4000;
 app.listen(port, () => {
   console.log(`Frontend en vivo en el puerto ${port}`);
+  console.log(`Sirviendo archivos desde: ${browserDistFolder}`);
 });
